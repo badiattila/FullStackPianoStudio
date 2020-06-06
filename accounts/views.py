75cb7690dc1bd6b17f383decf41a50f5906a4c04
Attila Badi <attila.badi@gmail.com>
2020-06-06 16:14:17 +0000

    Extending the user signup form to include First name, Last Name and Email fields

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views import generic
+from .forms import UserRegistrationForm
+from django.contrib import auth
 
 # We're subclassing the generic class-based view CreateView in our SignUp class. 
-# We specify the use of the built-in UserCreationForm and the template signup.html. 
+# We specify the use of UserRegistrationForm and the template signup.html. 
 # And we use reverse_lazy to redirect the user to the login page upon successful registration.
 
 class SignUp(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = UserRegistrationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
 
